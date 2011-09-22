dd6f47441f66624b76866d98784655541e1c88b8
unknown <ob@.jnmain00.corp.jndata.net>
2011-09-22 17:08:55 +0200

    hockeyGlue.checkForUpdates() still not working with just a Log.d statement :(

--- Android/HockeyApp/jqmHockeyApp/src/com/moxoed/pgPlugin/hockeyGlue/HockeyGlue.java
+++ Android/HockeyApp/jqmHockeyApp/src/com/moxoed/pgPlugin/hockeyGlue/HockeyGlue.java
@@ -1,28 +1,28 @@
 package com.moxoed.pgPlugin.hockeyGlue;
 
+/*
 import android.app.Activity;
 import net.hockeyapp.android.CheckUpdateTask;
-//import net.hockeyapp.android.UpdateActivity;
+*/
 import android.util.Log;
-import android.os.Bundle;
 
-public class HockeyGlue extends Activity{
-	private CheckUpdateTask checkUpdateTask;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+public class HockeyGlue {
+	
+//	private CheckUpdateTask checkUpdateTask;
 
-    	Log.d("HH","jj");
+    public void checkForUpdates() {
+    	Log.d("HockeyGlue", "checkForUpdates-did-I-get-here");
+/*
+         checkUpdateTask = (CheckUpdateTask)getLastNonConfigurationInstance();
+ 
+        if (checkUpdateTask != null) {
+          checkUpdateTask.attach(this);
+        }
+        else {
+          checkUpdateTask = new CheckUpdateTask(this, "https://rink.hockeyapp.net/", "dedae71020c1c014120ef0153cb8457c");
+          checkUpdateTask.execute();
+        }
+*/
     }
-	    public void checkForUpdates() {
-	    	Log.d("HockeyGlue", "checkForUpdates-did-I-get-here");
-	        checkUpdateTask = (CheckUpdateTask)getLastNonConfigurationInstance();
-	        if (checkUpdateTask != null) {
-	          checkUpdateTask.attach(this);
-	        }
-	        else {
-	          checkUpdateTask = new CheckUpdateTask(this, "https://rink.hockeyapp.net/", "dedae71020c1c014120ef0153cb8457c");
-	          checkUpdateTask.execute();
-	        }
-	    }
 }
 
