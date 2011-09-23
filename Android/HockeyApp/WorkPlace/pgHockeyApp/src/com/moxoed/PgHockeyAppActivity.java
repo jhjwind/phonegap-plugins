4c3013e2678e4c0a6790b595548d1bb417eac096
Owen Brotherwood <owen@brotherwood.biz>
2011-09-23 19:00:08 +0200

    Cut and paste of PhoneGap into Android Project: first attempt

--- Android/HockeyApp/WorkPlace/pgHockeyApp/src/com/moxoed/PgHockeyAppActivity.java
+++ Android/HockeyApp/WorkPlace/pgHockeyApp/src/com/moxoed/PgHockeyAppActivity.java
@@ -1,13 +1,13 @@
 package com.moxoed;
 
-import android.app.Activity;
+import com.phonegap.*;
 import android.os.Bundle;
 
-public class PgHockeyAppActivity extends Activity {
+public class PgHockeyAppActivity extends DroidGap {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
