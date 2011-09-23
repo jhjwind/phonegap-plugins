496e7b1ab12910c77e0150930eacd5f5a93f3fd7
Owen Brotherwood <owen@brotherwood.biz>
2011-09-24 00:35:14 +0200

    plugin java: basic Log.d

---
+++ Android/HockeyApp/pgHockeyApp/src/com/moxoed/pgPlugin/hockeyapp/HockeyApp.java
@@ -0,0 +1,19 @@
+package com.moxoed.pgPlugin.hockeyapp;
+
+import org.json.JSONArray;
+import android.util.Log;
+import com.phonegap.api.Plugin;
+import com.phonegap.api.PluginResult;
+import com.phonegap.api.PluginResult.Status;
+
+public class HockeyApp extends Plugin {
+	public PluginResult execute(String action, JSONArray data, String callbackId) {
+		Log.d("HockeyApp", "Plugin called");
+		PluginResult result = null;
+		result = new PluginResult(Status.OK);
+		return result;
+   }
+}
+
+
+ 
